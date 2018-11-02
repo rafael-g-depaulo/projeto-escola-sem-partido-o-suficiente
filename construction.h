7fce97005269705f0eab0869d99863ee55530721
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2018-11-02 16:16:17 -0300

    tentando entender o algoritmo

--- construction.h
+++ construction.h
@@ -1,4 +1,4 @@
-// Projeto 1 de Teoria e Aplicação de Grafos, Turma A
+// Projeto 3 de Teoria e Aplicação de Grafos, Turma A
 // Prof.: Díbio
 // Autores: Rafael Gonçalves de Paulo (17/0043959)
 //          João Lucas Azevedo Yamin Rodrigues da Cunha (17/0013731)
--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <queue>
 #include <vector>
 
+#define NUM_PARES 70
+
 using namespace std;
 
 int main() {
@@ -12,6 +14,25 @@ int main() {
   getProfsEscolas("entradaProj3TAG.txt", profs, escolas);
   vector<Adj> adj(escolas.length, pair<int, int>(-1, -1));
 
+  int paresQfaltam = NUM_PARES;
+  int controle[escolas.size()];
+  for (int i = 0; i < controle.size(); i++)
+    controle[i] = 0;
+
+  // aaaaaaaaaaaaaa
   vector<int> ans(profs.size(), -1);
   queue<int> auxiliarQueue;
+  for (int ind = 0; ind < escolas.size(); ind++)
+    auxiliarQueue.push_back(ind);
+
+  while (!auxiliarQueue.empty() && paresQfaltam > 0) {
+    int escAtual = auxiliarQueue.front();
+    int x = escolas[escAtual][controle[escAtual]];
+    int temp = -1;
+    int tRank = 100;
+    auxiliarQueue.pop_front();
+
+    for (int i : adj)
+
+  }
 }
